2e1f09b7542e7ff76126cd0c3ca3b8d918cb4b52
Brajesh Singh <brajesh@cosmiccoders.com>
2017-09-29 16:24:53 +0530

    Fix the member list for default layout

--- core/bp-featured-members-filters.php
+++ core/bp-featured-members-filters.php
@@ -7,27 +7,25 @@
  * @return mixed
  */
 function bp_featured_members_filter_members_list( $args ) {
-	//our scope must be featured
-	if ( ! isset( $args['scope'] ) || $args['scope'] != 'featured' ) {
-		return $args;
+	// our scope must be featured.
+	if (  bp_featured_members()->in_the_loop() || ( isset( $args['scope'] ) && $args['scope'] == 'featured' ) ) {
+
+		$args['meta_key']   = '_is_featured';
+		$args['meta_value'] = 1;
+
+		// which other params are we allowing?
+		$max         = bp_featured_members()->get( 'max' );
+		$member_type = bp_featured_members()->get( 'member_type' );
+
+		if ( $max ) {
+			$args['per_page'] = absint( $max );
+			$args['max']      = absint( $max );
+		}
+
+		if ( $member_type ) {
+			$args['member_type'] = $member_type;
+		}
 	}
-
-	$args['meta_key'] = '_is_featured';
-	$args['meta_value'] = 1;
-
-	//which other params are we allowing?
-	$max = bp_featured_members()->get( 'max' );
-	$member_type = bp_featured_members()->get( 'member_type' );
-
-	if ( $max ) {
-		$args['per_page'] = absint( $max );
-		$args['max'] = absint( $max );
-	}
-
-	if ( $member_type ) {
-		$args['member_type'] = $member_type;
-	}
-
 	return $args;
 }
 

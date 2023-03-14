cadae9192a2a3b84b453962482ea164fe5e2a10d
raviousprime <raviousprime@gmail.com>
2023-03-14 17:57:31 +0530

    version update

--- bp-featured-members.php
+++ bp-featured-members.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://buddydev.com/plugins/bp-featured-members/
  * Author: BuddyDev
  * Author URI: https://buddydev.com
- * Version: 1.1.3
+ * Version: 1.1.4
  * Text Domain: bp-featured-members
  * Domain Path: /languages
  */
